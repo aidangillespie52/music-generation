ff905bfbb1a5290aedaddad641cb18d5f7464234
Aidan Gillespie <aidangillespie52@gmail.com>
2025-02-02 17:47:34 -0600

    Removed sequences from the dataset that are at the end of a song and the beginning of the next song for training purposes

--- MusicSequenceDataset.py
+++ MusicSequenceDataset.py
@@ -1,25 +1,33 @@
 import torch
 from torch.utils.data import Dataset
 
-# TODO: Remove sequences in the dataset where the start and end values suddenly drop like when a song ends and another one starts
-
 class MusicSequenceDataset(Dataset):
     def __init__(self, notes, sequence_length):
-        self.notes = notes  # list of lists of dictionaries (list of sequences of notes)
-        self.sequence_length = sequence_length  # length of the input sequence
+        self.sequence_length = sequence_length
+        self.notes = self.filter_sequences(notes)
+
+    def filter_sequences(self, notes):
+        # Remove the sequences where one song is ending and another starts
+        filtered_notes = []
+        prev_start = None
+
+        for note in notes:
+            if prev_start is not None and note['start'] < prev_start:
+                continue  # Skip this note if a sudden drop occurs
+            
+            filtered_notes.append(note)
+            prev_start = note['start']
+
+        return filtered_notes
 
     def __len__(self):
-        return len(self.notes) - self.sequence_length  # Total number of sequences available
+        return len(self.notes) - self.sequence_length  
 
     def __getitem__(self, idx):
-        # Get a sequence of notes (list of dictionaries)
         sequence = self.notes[idx:idx + self.sequence_length]
         target = self.notes[idx + self.sequence_length]
 
-        # The sequence should now be a flat list of dictionaries, create the tensor for X
         X = torch.tensor([[note['start'], note['end'], note['pitch'], note['velocity']] for note in sequence], dtype=torch.float32)
-        
-        # The target note is the next note's features
         y = torch.tensor([target['start'], target['end'], target['pitch'], target['velocity']], dtype=torch.float32)
         
         return X, y
