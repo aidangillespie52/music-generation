0de8979aa5839b799627b462a4dc00f37ac10bf7
Aidan Gillespie <aidangillespie52@gmail.com>
2025-02-02 17:48:56 -0600

    Changed to look at the sequences instead of original data

--- MusicSequenceDataset.py
+++ MusicSequenceDataset.py
@@ -4,30 +4,37 @@ from torch.utils.data import Dataset
 class MusicSequenceDataset(Dataset):
     def __init__(self, notes, sequence_length):
         self.sequence_length = sequence_length
-        self.notes = self.filter_sequences(notes)
+        self.sequences = self.filter_sequences(notes)
 
+    def valid_sequence(self, start, seq):
+        for note in seq:
+            if note['start'] < start:  # Detected a sudden drop
+                return False
+            else:
+                start = note['start']
+
+        return True
+        
     def filter_sequences(self, notes):
-        # Remove the sequences where one song is ending and another starts
-        filtered_notes = []
-        prev_start = None
+        """Remove entire sequences that contain a drop in start time."""
+        res = []
 
-        for note in notes:
-            if prev_start is not None and note['start'] < prev_start:
-                continue  # Skip this note if a sudden drop occurs
+        for i, note in enumerate(notes[:len(notes) - self.sequence_length]):
+            start = note['start']
+            seq = notes[i:i+self.sequence_length]
+            valid_seq = self.valid_sequence(start, seq)
             
-            filtered_notes.append(note)
-            prev_start = note['start']
+            if valid_seq:
+                target = notes[i + self.sequence_length]
+                X = torch.tensor([[note['start'], note['end'], note['pitch'], note['velocity']] for note in seq], dtype=torch.float32)
+                y = torch.tensor([target['start'], target['end'], target['pitch'], target['velocity']], dtype=torch.float32)
+                res.append((X, y))
 
-        return filtered_notes
-
+        return res
+    
     def __len__(self):
-        return len(self.notes) - self.sequence_length  
+        return len(self.sequences)
 
     def __getitem__(self, idx):
-        sequence = self.notes[idx:idx + self.sequence_length]
-        target = self.notes[idx + self.sequence_length]
-
-        X = torch.tensor([[note['start'], note['end'], note['pitch'], note['velocity']] for note in sequence], dtype=torch.float32)
-        y = torch.tensor([target['start'], target['end'], target['pitch'], target['velocity']], dtype=torch.float32)
-        
+        X, y = self.sequences[idx]
         return X, y
